26d26aba87205d871515c6a171c568b0391a6096
celya_rousseau <celyarousseau.pro@gmail.com>
2014-03-27 14:02:00 +0100

    Add show flights [template+ controller]

--- src/AirAtlantique/FlightBundle/Controller/DefaultController.php
+++ src/AirAtlantique/FlightBundle/Controller/DefaultController.php
@@ -10,11 +10,39 @@ class DefaultController extends Controller
 {
    /*----------------------Recherche de vols-------------------------*/
     public function indexAction(){
-      $flight = new Flight;
-      $form = $this->createForm(new FlightType(), $flight);
+      // $flight = new Flight;
+      // $form = $this->createForm(new FlightType(), $flight);
+      $form = $this->createForm(new FlightType());
+      
+
+//On récupère la requête
+        $request = $this->getRequest();
+
+        if($request->getMethod() == 'POST')
+        {
+            $form->bind($request);
+
+            //On vérifie que les valeurs entrées sont correctes
+            if($form->isValid())
+            {
+                $em = $this->getDoctrine()->getManager();
+
+                //On récupère les données entrées dans le formulaire par l'utilisateur
+                $data = $this->getRequest()->request->get('airatlantique_flightbundle_flighttype');
+
+                //On va récupérer la méthode dans le repository afin de trouver toutes les annonces filtrées par les paramètres du formulaire
+                $flightList = $em->getRepository('FlightBundle:Flight')->findFlightByParameters($data);
+
+                //Puis on redirige vers la page de visualisation de cette liste d'annonces
+                return $this->render('FlightBundle::showFlights.html.twig', array('flightList' => $flightList));
+            }
+        }
+
+        // À ce stade :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
       return $this->render('FlightBundle::home.html.twig', array('form'=>$form->createView()));
     }
-
     // public function SearchFlightAction($form){
     //   $flightfiltered =$this-filterFlight($form);
     //   return $this->render('FlightBundle::home.html.twig', array('flightfiltered'=>$flightfiltered->createView()));
