68c3288da38f0771d88f185897ecbaedce7cc7b4
Tourman Benjamin <btourman@kaliterre.fr>
2014-03-31 21:01:19 +0200

    Remove BmatznerJQueryUIBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,7 +28,6 @@ class AppKernel extends Kernel
             new AirAtlantique\MailingBundle\MailingBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AirAtlantique\GeneratorBundle\GeneratorBundle(),
-            new Bmatzner\JQueryUIBundle\BmatznerJQueryUIBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
