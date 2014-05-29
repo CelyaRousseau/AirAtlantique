cd8ab356c81b52e3a7d021aaee3d7264a9fdbcba
KS-Krom <tourman@gmail.com>
2014-05-29 21:27:31 +0200

    [GeneratorBundle] - Add generate/delete method in the controller

--- src/AirAtlantique/GeneratorBundle/Controller/DefaultController.php
+++ src/AirAtlantique/GeneratorBundle/Controller/DefaultController.php
@@ -6,8 +6,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('GeneratorBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('GeneratorBundle:Default:index.html.twig');
+    }
+
+    function generateFlightsAction()
+    {
+        $utilFlight = $this->container->get('utilFlight');
+        $utilFlight->flightConstruct();
+        $utilFlight->ruleEngine();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function deleteFlightsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->getRepository('FlightBundle:Flight')->deleteAll();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function generateAirportsAction()
+    {
+        $utilFlight = $this->container->get('utilFlight');
+        $utilFlight->persistAirports();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function deleteAirportsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->getRepository('FlightBundle:Airport')->deleteAll();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function generatePlanesAction()
+    {
+        $utilFlight = $this->container->get('utilFlight');
+        $utilFlight->persistPlanes();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function deletePlanesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->getRepository('FlightBundle:Plane')->deleteAll();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function generateUsersAction()
+    {
+        $utilUser = $this->container->get('utilUser');
+        $utilUser->generateUser();
+        return $this->redirect($this->generateUrl('generator_homepage'));
+    }
+
+    function deleteUsersAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->getRepository('UserBundle:User')->deleteAll();
+        return $this->redirect($this->generateUrl('generator_homepage'));
     }
 }
